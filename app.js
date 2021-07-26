e7ccfc79e374cd4f6350a7ce743cf0549c1c9a7b
FranMazu <subzerov39@gmail.com>
2021-07-26 20:17:24 -0300

    edit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get("/home",(req,res)=>{
     res.sendFile(path.join(__dirname, "/views/home.html"))
 });
 
-app.listen( process.env.PORT || 3050,() => console.log("El hamster corre a 3000"));
+app.listen(process.env.PORT || 3050,() => console.log("El hamster corre a 3000"));
 
 app.get("/logIn",(req,res) => {
     res.sendFile(path.join(__dirname, "/views/logIn.html"))
