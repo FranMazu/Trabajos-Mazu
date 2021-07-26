4463f5463b725dec72c995852e7297f99089d0fc
FranMazu <subzerov39@gmail.com>
2021-07-26 19:34:22 -0300

    Subo mi proyecto

---
+++ MercadoLiebre/app.js
@@ -0,0 +1,21 @@
+const express=require("express");
+
+const path= require("path");
+
+const app = express();
+
+app.get("/home",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+});
+
+app.listen( process.env.PORT || 3050,() => console.log("El hamster corre a 3000"));
+
+app.get("/logIn",(req,res) => {
+    res.sendFile(path.join(__dirname, "/views/logIn.html"))
+});
+
+app.get("/register",(req,res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+})
+
+app.use(express.static("public"));
